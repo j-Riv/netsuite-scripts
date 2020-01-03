696f9901e1504e94b29f57ed0ee45bbf11d0eae7
j-Riv <jriv@suavecito.com>
2020-01-02 16:54:26 -0800

    Added SuiteScript 2.0 Version of Calculate Total Weight

---
+++ SuiteScripts/sales_order_user_event.js
@@ -0,0 +1,55 @@
+/**
+ * @NApiVersion 2.x
+ * @NScriptType UserEventScript
+ * @NModuleScope SameAccount
+ */
+define(['N/record'], function (record) {
+  function calculateTotalWeight(context) {
+    // get redcord
+    var currentRecord = record.load({
+      type: context.newRecord.type,
+      id: context.newRecord.id
+    });
+    // get line count
+    var lines = currentRecord.getLineCount({ sublistId: 'item' });
+    var totalWeight = 0;
+    var totalItems = 0;
+
+    for (var i = 0; i < lines; i++) {
+      // get weight unit (lb, oz, kg, g)
+      var unit = currentRecord.getSublistValue({ sublistId: 'item', fieldId: 'custcol_sp_item_weight_units', line: i });
+      var weight = currentRecord.getSublistValue({ sublistId: 'item', fieldId: 'custcol_sp_item_weight', line: i });
+      var quantity = currentRecord.getSublistValue({ sublistId: 'item', fieldId: 'quantity', line: i });
+
+      if (unit === 'oz') {
+        // convert oz to lbs
+        weight = weight * 0.0625;
+      } else if (unit === 'kg') {
+        // convert oz to kg
+        weight = weight * 2.20462;
+      } else if (unit === 'g') {
+        // convert oz to g
+        weight = weight * 0.00220462;
+      } else {
+        weight = weight * 1;
+      }
+
+      // calculate line weight
+      var lineWeight = weight * quantity;
+      // calculate total weight
+      totalWeight = parseFloat(totalWeight) + parseFloat(lineWeight);
+      // calculate total item count
+      totalItems = Number(totalItems) + Number(quantity);
+    }
+    // set fields
+    currentRecord.setValue({ fieldId: 'custbody_sp_total_items_weight',value: totalWeight });
+    currentRecord.setValue({ ieldId: 'custbody_sp_total_items',value: totalItems });
+    currentRecord.save({
+      enableSourcing: true,
+      ignoreMandatoryFields: false
+    });
+  }
+  return {
+    afterSubmit: calculateTotalWeight
+  }
+});
