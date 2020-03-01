629a9aa645ebeb13f9c9e49782e8549f744851ff
j-Riv <jriv@suavecito.com>
2020-03-01 13:12:15 -0800

    Update

---
+++ SuiteScripts/sales_order_ue.js
@@ -0,0 +1,35 @@
+/**
+ * @NApiVersion 2.1
+ * @NScriptType UserEventScript
+ * @NModuleScope SameAccount
+ */
+define(['N/record', 'N/log'], function (record, log) {
+
+  function beforeSubmit(context) {
+    var currentRecord = context.newRecord;
+    var marketplace = currentRecord.getValue({ fieldId: 'custbody_fa_channel' });
+    var salesRep;
+    if (marketplace == 'Shopify') {
+      // salesRep = 'Online Store';
+      salesRep = 73559;
+    }
+    if (marketplace == 'Shopify-WholesaleShopify') {
+      // salesRep = 'Partner Store';
+      salesRep = 73560;
+    }
+    if (marketplace.includes('Amazon')) {
+      // salesRep = 'Amazon Store';
+      salesRep = 73562;
+    }
+    if (marketplace == 'eBay') {
+      // salesRep = 'eBay Store';
+      salesRep = 73561;
+    }
+    // set sales rep
+    currentRecord.setValue({ fieldId: 'salesrep', value: salesRep });
+  }
+
+  return {
+    beforeSubmit: beforeSubmit
+  }
+});
