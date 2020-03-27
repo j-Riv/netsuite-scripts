c403368a984b7fe746965fb2ed78beac7b908c99
j-Riv <jriv@suavecito.com>
2020-03-26 17:21:36 -0700

    Updates to FedEx 2Day Express

--- SuiteScripts/item_fulfillment_client_BACKUP.js
+++ SuiteScripts/item_fulfillment_client_BACKUP.js
@@ -5,13 +5,8 @@
 define(['N/currentRecord', 'N/ui/dialog', 'N/log'],
   function (currentRecord, dialog, log) {
     // fedex expreess 2day
-    // production: 30611 | Sandbox: 30588
-    /**
-     * On page load it checks the item fulfillment ship method
-     * if method is FedEx 2 Day Express then it checks the status,
-     * depending on the status it automatically selects the 
-     * appropriate fields.
-     */
+    // production: 30611
+    // sandbox: 30588
     function pageInit() {
       // on page load
       console.log('FedEx Client Script Loaded');
@@ -19,25 +14,23 @@ define(['N/currentRecord', 'N/ui/dialog', 'N/log'],
       var status = itemFulfill.getValue('shipstatus');
       // check if fedex express 2day
       if (itemFulfill.getValue('shipmethod') == '30611') {
-        // check day
-        var date = new Date();
-        var day = date.getDay();
-        console.log('Day: ' + day);
         try {
-          // if status picked or packed
-          if (status == 'A' || status == 'B') {
-            itemFulfill.setValue('shipstatus', 'C');
+          // if status: picked
+          if (status == 'A') {
+            itemFulfill.setValue('shipstatus', 'B');
             itemFulfill.setValue('generateintegratedshipperlabel', true);
-            // if thursday set saturday delivery
-            if (day == '4') {
-              itemFulfill.setValue('saturdaydeliveryfedex', true);
-              var msg = 'Saturday Delivery has been selected and the status has changed to SHIPPED. Please print the label.';
-            } else {
-              var msg = 'Status has changed to SHIPPED. Please print the label.';
-            }
             dialog.alert({
               title: 'Ship Status Changed',
-              message: msg
+              message: 'Ship status has changed from Picked to PACKED. Please save record and press mark shipped. Then print label.'
+            });
+          }
+          // if status: packed
+          if (status == 'B') {
+            itemFulfill.setValue('shipstatus', 'B');
+            itemFulfill.setValue('generateintegratedshipperlabel', true);
+            dialog.alert({
+              title: 'Ship Status Changed',
+              message: 'Please save record and press mark shipped. Then print label.'
             });
           }
         } catch (e) {
@@ -46,14 +39,10 @@ define(['N/currentRecord', 'N/ui/dialog', 'N/log'],
       }
     }
 
-    /**
-     * Automates the manual process of selecting FedEx 2 Day Express
-     */
     function fedexExpress() {
       console.log('FedEx Express button selected');
       var itemFulfill = currentRecord.get();
       console.log(itemFulfill);
-
       try {
         // change shipment method -- reloads
         var shipmethod = itemFulfill.getValue('shipmethod');
@@ -62,7 +51,7 @@ define(['N/currentRecord', 'N/ui/dialog', 'N/log'],
         if (shipmethod != '30611') {
           itemFulfill.setValue('shipmethod', '30611');
         }
-        itemFulfill.setValue('shipstatus', 'C');
+        itemFulfill.setValue('shipstatus', 'B');
         itemFulfill.setValue('generateintegratedshipperlabel', true);
 
       } catch (e) {
