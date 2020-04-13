3c21263871aba0bab4f77c45dfedf1e9ef01cd56
j-Riv <jriv@suavecito.com>
2020-04-13 16:58:47 -0700

    Updates: Added Suave After Dark

--- SuiteScripts/item_fulfillment_client.js
+++ SuiteScripts/item_fulfillment_client.js
@@ -11,6 +11,9 @@ define(['N/currentRecord', 'N/ui/dialog', 'N/log'],
      * if method is FedEx 2 Day Express then it checks the status,
      * depending on the status it automatically selects the 
      * appropriate fields.
+     * Ex:
+     * - Sets Saturday delivery if ship day is Thursday.
+     * - Sets ship date as next business day if shipping on a Saturday or after 4:45pm.
      */
     function pageInit() {
       // on page load
@@ -19,10 +22,14 @@ define(['N/currentRecord', 'N/ui/dialog', 'N/log'],
       var status = itemFulfill.getValue('shipstatus');
       // check if fedex express 2day
       if (itemFulfill.getValue('shipmethod') == '30611') {
+        var isShipNextDay = '';
+        var shipDay;
         // check day
         var date = new Date();
         var day = date.getDay();
-        console.log('Day: ' + day);
+        var hour = date.getHours();
+        var minutes = date.getMinutes();
+        console.log('Day: ' + day + ' | Hour: ' + hour);
         try {
           // if status picked or packed
           if (status == 'A' || status == 'B') {
@@ -31,13 +38,21 @@ define(['N/currentRecord', 'N/ui/dialog', 'N/log'],
             // if thursday set saturday delivery
             if (day == '4') {
               itemFulfill.setValue('saturdaydeliveryfedex', true);
-              var msg = 'Saturday Delivery has been selected and the status has changed to SHIPPED. Please print the label.';
+              var msg = 'Saturday Delivery has been selected and the status has changed to SHIPPED.';
+            } else if (day == '6') {
+              shipDay = shipNextDay(true);
+              isShipNextDay = ' Since it\'s a Saturday, the ship date has been changed automatically to: ' + shipDay + ' Make sure it\'s correct.';
             } else {
-              var msg = 'Status has changed to SHIPPED. Please print the label.';
+              if (hour >= '16' && minutes >= '45') {
+                shipDay = shipNextDay(true);
+                isShipNextDay = ' It\'s after 4:45 PM the ship date has been changed automatically to: ' + shipDay + ' Make sure it\'s correct.';
+              }
+              var msg = 'Status has changed to SHIPPED.';
             }
+
             dialog.alert({
               title: 'Ship Status Changed',
-              message: msg
+              message: msg + isShipNextDay + ' Please save and print the label.'
             });
           }
         } catch (e) {
@@ -53,6 +68,10 @@ define(['N/currentRecord', 'N/ui/dialog', 'N/log'],
       console.log('FedEx Express button selected');
       var itemFulfill = currentRecord.get();
       console.log(itemFulfill);
+      // check day
+      var date = new Date();
+      var day = date.getDay();
+      console.log('Day: ' + day);
 
       try {
         // change shipment method -- reloads
@@ -67,8 +86,55 @@ define(['N/currentRecord', 'N/ui/dialog', 'N/log'],
       }
     }
 
+    /**
+     * Changes the carrier settings for next day shipping, based on the next business day.
+     */
+    function shipNextDay(hideDialog) {
+      console.log('Next Day Shipping has been selected');
+      console.log('FedEx Client Script Loaded');
+      var itemFulfill = currentRecord.get();
+      // check if fedex express 2day
+      if (itemFulfill.getValue('shipmethod') == '30611') {
+        try {
+          var today = new Date();
+          var shipDay = new Date(today);
+          var daysToAdd;
+          var day = shipDay.getDay();
+          if (day == 5) {
+            daysToAdd = 3;
+          } else if (day == 6) {
+            daysToAdd = 2;
+          } else {
+            daysToAdd = 1;
+          }
+
+          shipDay.setDate(shipDay.getDate() + daysToAdd);
+          shipDay.setHours(0);
+          shipDay.setMinutes(0);
+          shipDay.setSeconds(0);
+          itemFulfill.setValue('shipdatefedex', shipDay);
+
+          if (!hideDialog) {
+            dialog.alert({
+              title: 'Ship Date Changed',
+              message: 'The ship date has been changed to: ' + shipDay + ' Please make sure it\'s correct and save.'
+            });
+          }
+          return shipDay;
+        } catch (e) {
+          console.log(e.message);
+        }
+      } else {
+        dialog.alert({
+          title: 'Error:',
+          message: 'In order to change the ship date you must first select FedEx 2Day Express as the ship method.'
+        });
+      }
+    }
+
     return {
       pageInit: pageInit,
-      fedexExpress: fedexExpress
+      fedexExpress: fedexExpress,
+      shipNextDay: shipNextDay
     };
   }); 
