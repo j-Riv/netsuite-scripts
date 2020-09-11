e06fff32d4964d8336eedc9982f2f1341934db6b
j-Riv <jriv@suavecito.com>
2020-09-11 16:20:24 -0700

    Refactored Cash Refund Client

--- SuiteScripts/cash_refund_client.js
+++ SuiteScripts/cash_refund_client.js
@@ -19,68 +19,59 @@ define(['N/currentRecord', 'N/ui/dialog', 'N/log'],
 
       // If the Customer is Taxable & FarApp Order Data exist show the dialog
       if (typeof faData != 'undefined' && isTaxable && totalTax != '') {
+        var faShipTaxRate = parseFloat(cashRefund.getValue('custbody_fa_shipping_tax'));
+        var faShipTax = parseFloat(faData.shippingTax);
+        var msg = 'This is an online order. ';
+
+        if (!isNaN(faShipTaxRate) && faShipTax > 0) {
+          msg += 'This order has a Shipping Tax Rate but no Tax Code. Please use the "Update Tax / Total" button before saving the record. ';
+        }
+
+        msg += 'If your processing a full refund, please make sure the Cash Refund total matches the Cash Sale ' +
+          'and or Sales Order attached before saving. If it does not please use the "Update Tax / Total" button.';
+
         dialog.alert({
           title: 'TAXABLE ONLINE ORDER',
-          message: 'Please make sure the Cash Refund total matches the Cash Sale ' + 
-            'and or Sales Order attached before saving. If it does not please use the "Update Tax / Total" button.'
+          message: msg
         });
       }
     }
 
     /**
-     * Sets / Updates the total tax and refund total to match the attached
-     * Cash Sale and or Sales Orders. NetSuite & Shopify / FarApp have different
-     * tax rates. Sometimes these tax rates don't match. FarApp overwrites the
-     * transaction tax rate and uses w/e Shopify sent. During a Cash Refund 
-     * sometimes the orders don't match because NetSuite will re-calculate this
-     * tax rate. This is a problem.
+     * Sets / Updates the shipping tax code, shipping tax rate, total tax & 
+     * refund total to match the attached Cash Sale & or Sales Order. NetSuite &
+     * Shopify / FarApp calculate tax rates differently. Sometimes these tax rates
+     * dont't match. FarAp overwrites the transaction tax rate and uses w/e Shopify
+     * sent. During a Cash Refund sometimes the orders don't match because NetSuite
+     * will re-calculate this tax rate. Other times it's because Shopify calculated
+     * shipping tax while NetSuite did not. This is a problem
      */
-    function setTax() {
+    function updateRefund() {
       var cashRefund = currentRecord.get();
-      var faOrder = cashRefund.getValue('custbody_fa_channel_order');
       var isTaxable = cashRefund.getValue('custbody_sp_customer_is_taxable');
-      var total = cashRefund.getValue('total');
-      var totalTax = cashRefund.getValue('taxtotal');
 
       if (isTaxable) {
-        // get FarApp Data -- this is usually the problem
+        var total = parseFloat(cashRefund.getValue('total'));
+        var totalTax = parseFloat(cashRefund.getValue('taxtotal'));
+        // get FarApp Data
         var faData = JSON.parse(cashRefund.getValue('custbody_fa_order_total'));
-        var faTotal = faData.orderTotal;
         var faShipTaxRate = parseFloat(cashRefund.getValue('custbody_fa_shipping_tax'));
-        var faTaxTotal = 0;
+        var faShipTax = parseFloat(faData.shippingTax);
 
-        log.debug({
-          title: 'CURRENT VALUES',
-          details: 'Order: ' + faOrder + ' | isTaxable: ' + isTaxable + ' | totalTax: ' + totalTax + ' | total: ' + total
-        });
-
-        // set tax total if it exists
-        if (faData.taxTotal) { faTaxTotal = faData.taxTotal; }
-        // update tax
-        if (parseFloat(totalTax) != parseFloat(faTaxTotal)) {
+        if (!isNaN(faShipTaxRate)) {
           if (parseFloat(cashRefund.getValue('shippingtax1rate')) != faShipTaxRate) {
             var taxCode = cashRefund.getSublistValue({ sublistId: 'item', fieldId: 'taxcode', line: 0 });
             cashRefund.setValue({ fieldId: 'shippingtaxcode', value: taxCode, ignoreFieldChange: true });
             cashRefund.setValue({ fieldId: 'shippingtax1rate', value: faShipTaxRate });
+            cashRefund.setValue({ fieldId: 'taxtotal', value: totalTax + faShipTax });
+            cashRefund.setValue({ fieldId: 'total', value: total + faShipTax });
           }
-          cashRefund.setValue({ fieldId: 'taxtotal', value: faTaxTotal });
-        }
-        // update total
-        if (parseFloat(total) != parseFloat(faTotal)) {
-          cashRefund.setValue({ fieldId: 'total', value: faTotal });
-        }
-
-        if (parseFloat(cashRefund.getValue('shippingtax1rate')) != faShipTaxRate) {
-          console.log('run again');
-          return setTax();
-        } else {
-          console.log('dont run again');
         }
 
         // display dialog
         dialog.alert({
           title: 'Tax / Total Updated',
-          message: 'Please make sure the Cash Refund total matches the Cash Sale and or Sales Order attached before saving.'
+          message: 'If your processing a full refund, please make sure the Cash Refund total matches the Cash Sale and or Sales Order attached before saving.'
         });
       } else {
         // not taxable, what are you doing :/
@@ -93,6 +84,6 @@ define(['N/currentRecord', 'N/ui/dialog', 'N/log'],
 
     return {
       pageInit: pageInit,
-      setTax: setTax
+      updateRefund: updateRefund
     };
   });
