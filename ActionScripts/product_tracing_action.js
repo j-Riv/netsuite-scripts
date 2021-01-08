9b3c2c5abe13aab5ccd43ac9cbd077ac28fc5007
j-Riv <jriv@suavecito.com>
2021-01-08 11:21:43 -0800

    update

--- ActionScripts/product_tracing_action.js
+++ ActionScripts/product_tracing_action.js
@@ -20,11 +20,19 @@ define(['N/record'],
 
       for (let i = 0; i < lines; i++) {
         const id = parseInt(salesRecord.getSublistValue({ sublistId: 'item', fieldId: 'custcol_sp_item_id', line: i }));
-
+        // singles
         if (id === 24867 || id === 24874) {
           quantity += parseInt(salesRecord.getSublistValue({ sublistId: 'item', fieldId: 'quantity', line: i }));
         }
-
+        // 3
+        if (id === 30596 || id === 30597) {
+          quantity += parseInt(salesRecord.getSublistValue({ sublistId: 'item', fieldId: 'quantity', line: i })) * 3;
+        }
+        // 5
+        if (id === 30598 || id === 30609) {
+          quantity += parseInt(salesRecord.getSublistValue({ sublistId: 'item', fieldId: 'quantity', line: i })) * 5;
+        }
+        // 12
         if (id === 30154 || id === 30155) {
           quantity += parseInt(salesRecord.getSublistValue({ sublistId: 'item', fieldId: 'quantity', line: i })) * 12;
         }
